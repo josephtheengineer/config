e6528018739f45a1f5cf83a4c78e4121e8c475bd
JosephTheEngineer <jiuzzolino@outlook.com>
2019-02-16 17:56:28 +1000

    Added rofi config

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
-rm ~/.zshrc.config
+rm ~/.zshrc
 ln -s ~/dotfiles/zsh.config ~/.zshrc
 rm ~/.config/sway/config
 ln -s ~/dotfiles/sway.config ~/.config/sway/config
+rm ~/.config/rofi/config
+ln -s ~/dotfiles/rofi.config ~/.config/rofi/config
