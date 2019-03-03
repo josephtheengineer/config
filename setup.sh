dc6a32f7c12de24181994ddecaacf609e342f7d0
JosephTheEngineer <jiuzzolino@outlook.com>
2019-03-03 18:33:40 +1000

    added gtk.config

--- setup.sh
+++ setup.sh
@@ -6,3 +6,5 @@ rm ~/.config/rofi/config
 ln -s ~/dotfiles/rofi.config ~/.config/rofi/config
 rm ~/.config/termite/config
 ln -s ~/dotfiles/termite.config ~/.config/termite/config
+rm ~/.config/gtk-3.0/gtk.css
+ln -s ~/dotfiles/gtk.config ~/.config/gtk-3.0/gtk.css
