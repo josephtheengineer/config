a9b84af2e7128228e75dda7cf12b663e6dc1ee31
JosephTheEngineer <jiuzzolino@outlook.com>
2019-03-06 18:00:58 +1000

    Added install-os.sh

--- setup.sh
+++ setup.sh
@@ -6,3 +6,9 @@ rm ~/.config/rofi/config
 ln -s ~/dotfiles/rofi.config ~/.config/rofi/config
 rm ~/.config/termite/config
 ln -s ~/dotfiles/termite.config ~/.config/termite/config
+
+# oh my zsh
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+#wget -P ~/fonts https://github.com/belluzj/fantasque-sans/releases/download/v1.7.2/FantasqueSansMono-Normal.tar.gz
+
