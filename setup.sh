c9f2b58ab1571e39de34930fc03779f51b24aa05
josephtheengineer <jiuzzolino@outlook.com>
2019-04-23 06:49:42 +1000

    Changes

--- setup.sh
+++ setup.sh
@@ -1,19 +1,21 @@
 rm ~/.zshrc
-ln -s ~/dotfiles/zsh.config ~/.zshrc
-rm ~/.config/sway/config
-ln -s ~/dotfiles/sway.config ~/.config/sway/config
+ln -s ~/config/zsh.conf ~/.zshrc
+#rm ~/.config/sway/config
+#ln -s ~/config/sway.conf ~/.config/sway/config
+
+cat $HOME/config/sway.conf \
+    $HOME/config/sway.local.conf > $HOME/.config/sway/config
+
+
 rm ~/.config/rofi/config
-ln -s ~/dotfiles/rofi.config ~/.config/rofi/config
+ln -s ~/config/rofi.conf ~/.config/rofi/config
 rm ~/.config/termite/config
-ln -s ~/dotfiles/termite.config ~/.config/termite/config
-<<<<<<< HEAD
+ln -s ~/config/termite.conf ~/.config/termite/config
 
 # oh my zsh
 #sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 #wget -P ~/fonts https://github.com/belluzj/fantasque-sans/releases/download/v1.7.2/FantasqueSansMono-Normal.tar.gz
 
-=======
 rm ~/.config/gtk-3.0/gtk.css
-ln -s ~/dotfiles/gtk.config ~/.config/gtk-3.0/gtk.css
->>>>>>> d4e549524fa2c6765e8a444361bfbc6889011e77
+ln -s ~/dotfiles/gtk.conf ~/.config/gtk-3.0/gtk.css
