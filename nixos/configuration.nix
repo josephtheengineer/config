c9d8bb38347dc458accf6380d3b4e2d4afd679b8
josephtheengineer <jiuzzolino@outlook.com>
2019-04-25 06:06:03 +1000

    Changed from symlink system to .config folders

---
+++ nixos/configuration.nix
@@ -0,0 +1,225 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      ./local-configuration.nix
+    ];
+
+  boot = {
+    kernelPackages = pkgs.linuxPackages_latest; 
+    loader.grub.enable = false;
+
+    # Use the systemd-boot EFI boot loader.
+    loader.systemd-boot.enable = true;
+    loader.efi.canTouchEfiVariables = true;
+  };
+
+  networking = {
+    hostName = "yoshi";
+    networkmanager.enable = true;
+
+    # Open ports in the firewall.
+    firewall.allowedTCPPorts = [ ];
+    firewall.allowedUDPPorts = [ ];
+
+    #proxy.default = "http://user:password@proxy:port/";
+    #proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  };
+
+  hardware = {
+    # Steam 32 bit stuff
+    opengl.driSupport32Bit = true;
+    pulseaudio.support32Bit = true;
+
+    pulseaudio.enable = true;   
+  };
+
+  system = {
+    autoUpgrade.enable = true;
+    autoUpgrade.channel = "https://nixos.org/channels/nixos-unstable";
+    
+  };
+
+  services = {
+    openssh.enable = true;
+    printing.enable = true;
+    printing.drivers = [ pkgs.gutenprint  ];
+  };
+
+  programs = {
+    dconf.enable = true;
+    light.enable = true;
+    sway.enable = true;
+    zsh.enable = true;
+    zsh.histFile = "$XDG_DATA_HOME";
+    zsh.ohMyZsh.enable = true;
+    zsh.shellInit = "echo woah";
+    zsh.syntaxHighlighting.enable = true;
+    
+  };
+
+  nix = {
+    gc.automatic = true;
+    gc.dates = "12:00";
+
+    extraOptions = ''
+      connect-timeout = 10
+    '';
+  };
+  users.users = {
+    josephtheengineer = {
+      isNormalUser = true;
+      home = "/home/josephtheengineer";
+      description = "admin";
+      extraGroups = [ "wheel" "libvirtd" "sway" "networkmanager" "video" ];
+      shell = pkgs.zsh;
+      uid = 1000;
+    };
+    eco = {
+      isNormalUser = true;
+      home = "/home/eco";
+      description = "awesome";
+      extraGroups = [];
+      shell = pkgs.zsh;
+    };
+  };
+
+  security.pki.certificates =   [ ''
+				NixOS.org
+				=========
+				------BEGIN CERTIFICATE-------
+				HJYTJKHDKJFHKDF768302HJHJG26825JKHG68HJKJJHKGTF
+				976KHYTUDKJJHFDSJKFD768030KJH63KJGJGG6383GJKDHJ
+				-----END CERTIFICATE--------
+				'' 
+				];
+
+  time.timeZone = "Australia/Brisbane";
+  nixpkgs.config.allowUnfree = true;
+  virtualisation.libvirtd.enable = true;
+  sound.enable = true;
+
+  # Enable the i3-gaps Desktop Environment.
+  #services.xserver = {
+    #enable = true;
+    #displayManager.startx.enable = true;
+    #displayManager.lightdm.enable = false;
+    #desktopManager = {
+      #default = "none";
+      #xterm.enable = false;
+    #};
+    #windowManager.i3.package = pkgs.i3-gaps;
+    #windowManager.i3.enable = true;
+    #videoDrivers = [ "nvidia" ];
+  #};
+
+  # Virtualbox
+  #virtualisation.virtualbox.host.enable = true;
+  #users.extraGroups.vboxusers.members = [ "josephtheengineer" ];
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    wget
+    vim
+    emacs
+    htop
+    qutebrowser
+    neofetch
+    gotop
+    barrier
+    gimp
+    nodejs
+    arc-theme
+    gtk-engine-murrine
+    arc-icon-theme
+    breeze-gtk
+    capitaine-cursors
+    powerline-fonts
+    zsh
+    git
+    git-lfs
+    gnome3.gnome-disk-utility
+    pavucontrol
+    roboto-mono
+    gnome3.gnome-font-viewer
+    xfce.tumbler
+    ranger
+    python3
+    mono
+    firefox
+    cmake
+    clang_39
+    unzip
+    shutter
+    pywal
+    nix-prefetch-git
+    zip
+    stdenv
+    bcache-tools
+    weechat
+    winetricks
+    figlet
+    lolcat
+    i3blocks-gaps
+    rtorrent
+    libvirt
+    virtmanager
+    qemu
+    networkmanager
+    networkmanagerapplet
+    cryptsetup
+    lvm2
+    xorg.xrdb
+    iw
+    iwd
+    qemu_kvm
+    capitaine-cursors
+    parted
+    i3blocks
+    ffmpeg
+    discord
+    adoptopenjdk-bin
+    killall
+    connman-ncurses
+    connmanui
+    connman-gtk
+    arc-icon-theme
+    ppp
+    pptp
+    rofi
+    source-sans-pro
+    source-code-pro
+    godot
+    libreoffice
+    system-config-printer
+    termite
+    w3m
+    gnumake
+    youtube-dl
+    google-chrome
+    tigervnc
+    conky
+    kitty
+    fortune
+    cowsay
+    bsdgames
+    gnupg
+    mpd
+    ncmpcpp
+    mpc_cli 
+    file
+    acpi
+    groff
+  ];
+
+  # Determines the NixOS release with which your system is to be compatible.
+  # You should change this only after the release notes say you should.
+  system.stateVersion = "18.09"; # Did you read the comment?
+}
--- setup.sh
+++ setup.sh
@@ -1,21 +1,8 @@
-rm ~/.zshrc
-ln -s ~/config/zsh.conf ~/.zshrc
-#rm ~/.config/sway/config
-#ln -s ~/config/sway.conf ~/.config/sway/config
+ln -s ~/.config/nixos/configuration.nix /etc/nixos/configuration.nix
+ln -s ~/.config/nixos/local-configuration.nix /etc/nixos/local-configuration.nix
 
-cat $HOME/config/sway.conf \
-    $HOME/config/sway.local.conf > $HOME/.config/sway/config
+cat ~/.config/sway/main \
+    ~/.config/sway/local > ~/.config/sway/config
 
-
-rm ~/.config/rofi/config
-ln -s ~/config/rofi.conf ~/.config/rofi/config
-rm ~/.config/termite/config
-ln -s ~/config/termite.conf ~/.config/termite/config
-
-# oh my zsh
-#sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 #wget -P ~/fonts https://github.com/belluzj/fantasque-sans/releases/download/v1.7.2/FantasqueSansMono-Normal.tar.gz
-
-rm ~/.config/gtk-3.0/gtk.css
-ln -s ~/dotfiles/gtk.conf ~/.config/gtk-3.0/gtk.css
