0c131bfa9ecc96a4f8d6c2f892d62c80d64272a2
josephtheengineer <josephtheengineer@pm.me>
2019-09-09 17:11:13 +1000

    added qutebrowser.sh

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -69,6 +69,13 @@ in
 			enable = true;
 			drivers = [ pkgs.gutenprint ];
 		};
+		#paperless = {
+		#	enable = true;
+		#	consumptionDir = "/var/spool/scans/";
+		#	extraConfig = {
+		#		PAPERLESS_CORS_ALLOWED_HOSTS="http://localhost:8080";
+		#	};
+		#};
 	};
 
 	programs = {
@@ -101,7 +108,7 @@ in
 			isNormalUser = true;
 			home = "/home/josephtheengineer";
 			description = "admin";
-			extraGroups = [ "wheel" "libvirtd" "sway" "networkmanager" "video" ];
+			extraGroups = [ "wheel" "libvirtd" "sway" "networkmanager" "video" "scanner" "lp" ];
 			shell = pkgs.zsh;
 			uid = 1000;
 		};
@@ -251,6 +258,11 @@ cvYrtmo4ql4TaI9ssx31VlCAgaK0XEdlDZ6R+A==
 		telnet
 		pass
 		bind
+		quakespasm
+		patchelf
+		saneBackends
+		#paperless
+		figlet
 	];
 
 	fonts = {
