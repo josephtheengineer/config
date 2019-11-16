d263137362f184c7cffde4c37b1152146d792ca7
josephtheengineer <joseph@theengineer.life>
2019-11-16 11:28:54 +1000

    fixed status bar error

--- lib/scripts/status.sh
+++ lib/scripts/status.sh
@@ -4,7 +4,7 @@ net="disconnected"
 vol=0
 date="0000-00-00 00:00:00"
 
-+while echo " net:$net | vol: $vol | $date UTC$timezone"
+while echo " net:$net | vol: $vol | $date UTC$timezone"
 	do sleep 1
 	vol=$(pactl list sinks | grep '^[[:space:]]Volume:' | head -n $(( $SINK + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,')
 
