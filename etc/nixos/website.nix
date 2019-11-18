9829b375b5af5e899637bacbf3ef2f09627fb6ad
josephtheengineer <josephtheengineer@pm.me>
2019-11-18 16:19:18 +1000

    Server stuff

--- etc/nixos/website.nix
+++ etc/nixos/website.nix
@@ -6,14 +6,12 @@
  		statusPage = true;
  		virtualHosts."www.theengineer.life" = {
 			#addSSL = true;
-			#enableACME = true;
-			#index index.html;
-			#autoindex on;
+			enableACME = true;
 			root = "/srv/www/theengineer.life";
-			listen = [ { addr = "*"; port = 8787; } ];
+			#listen = [ { addr = "*"; port = 8787; } ];
 		};
-		#virtualHosts."eco.theengineer.life" = {
-			#globalRedirect = "elicox.tech";
-		#};
+		virtualHosts."genesis.theengineer.life" = {
+			globalRedirect = "www.theengineer.life";
+		};
 	};
 }
